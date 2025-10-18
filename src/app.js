db51692ead9400d391308a779a77242fd6135328
Mohit-Kumar98 <92840979+Mohit-Kumar98@users.noreply.github.com>
2025-10-18 17:27:57 +0530

    Exploring Routes in Express

--- src/app.js
+++ src/app.js
@@ -3,17 +3,28 @@ const express = require('express');
 const app = express();
 
 
-app.use("/test",(req,res)=>{
-   res.send("Hello World");
+// This will only handle GET call to the /.
+app.get("/user",(req,res)=>{
+   res.send({firstname:"Mohit",lastname:"Kumar"});
 })
 
+app.post("/user",(req,res)=>{
+    // Save data to DB.
+    res.send("Data successfully saved to the database");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("User Deleted Successfully");N  
+})
+
+// This will match all the HTTP method API calls to the /hello.
 app.use("/hello",(req,res)=>{
-   res.send("Hello from DevTinder");
+   res.send("Hello world ");
 })
 
 
-    app.listen(3000,()=>{
-        console.log("Server is running on port 3000");  
-    });
 
-    app
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");  
+});
