f063993e087345f8a9ad6a109aeb224cab4351fe
Mohit-Kumar98 <92840979+Mohit-Kumar98@users.noreply.github.com>
2025-10-20 22:13:56 +0530

    creating api's

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const connectDB=require('./config/database');
 const app = express();
 const UserModel=require('./models/user');
 
+// This will run on every request that will come to the Server. middleware to convert json into js object.
+app.use(express.json());
+
 
 app.get("/user",(req,res)=>{
     res.send("User present");
@@ -10,23 +13,46 @@ app.get("/user",(req,res)=>{
 
 
 app.post("/signup",async (req,res)=>{
-    console.log("Before data save");
+    
     // We are creating new instance of the User model. and we are passing the data to that instance.
-    const user=new UserModel({
-        firstName:"Mk",
-        lastName:"Kumar",
-        emailId:"mk@gmail.com",
-        password:"mohit@1234"
-    });
-
+    const user=new UserModel(req.body);
+      console.log(req.body);
     try{
       await user.save();
       res.send("User Created Successfully"); 
     }catch(err){
-      console.log("Something went wrong");
+      console.log("Error Saving the user:"+ err.messge);
     }
-    
+
 });
+
+app.get("/feed",async (req,res)=>{
+
+  try{
+    const users=await UserModel.find({});
+    if(users.length===0){
+      res.status(404).send("User not found.");
+    }else{
+      res.send(users);
+    }
+
+  }catch(err){
+      console.log("Error Saving the user:"+ err.messge);
+  }
+   
+})
+
+app.delete("/deleteUser", async(req,res)=>{
+
+  try{
+    const deletedata=req.body.firstName;
+    await UserModel.deleteOne({firstName:deletedata});
+    res.send("User Deleted Successfully.");
+  }catch(err){
+      console.log("Error Saving the user:"+ err.messge);
+  }
+    
+})
 
 connectDB().then(()=>{
   console.log("DB connected")
