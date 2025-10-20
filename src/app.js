3e071f06c3e9cb2f857cb636e65e594bcca44c8d
Mohit-Kumar98 <92840979+Mohit-Kumar98@users.noreply.github.com>
2025-10-20 17:36:00 +0530

    connecting our application to database

--- src/app.js
+++ src/app.js
@@ -1,36 +1,44 @@
 const express = require('express');
+const connectDB=require('./config/database');
 const app = express();
-const {adminAuth,userAuth}=require('./middlewares/auth')
+const UserModel=require('./models/user');
 
-app.use("/admin",adminAuth);
 
-app.post("/user/login",(req,res)=>{
-    res.send("User logged in Successfully!");
+app.get("/user",(req,res)=>{
+    res.send("User present");
 })
 
-app.get("/user/data",userAuth,(req,res)=>{
-    throw new Error("Error with user");
-    res.send("User data send")
-})
 
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("All Data sent");
-})
+app.post("/signup",async (req,res)=>{
+    console.log("Before data save");
+    // We are creating new instance of the User model. and we are passing the data to that instance.
+    const user=new UserModel({
+        firstName:"Mk",
+        lastName:"Kumar",
+        emailId:"mk@gmail.com",
+        password:"mohit@1234"
+    });
 
-app.get("/admin/deleteUser",(req,res)=>{
-    res.send("Deleted a user");
-})
-
-app.use("/",(err,req,res,next)=>{
-    if(err){
-        // Log error
-        res.status(500).send("Something went wrong");
-    }else{
-        next();
+    try{
+      await user.save();
+      res.send("User Created Successfully"); 
+    }catch(err){
+      console.log("Something went wrong");
     }
+    
+});
+
+connectDB().then(()=>{
+  console.log("DB connected")
+  app.listen(3000,()=>{
+    console.log("Server is running on port 3000");  
+})  ;  
+}).catch((err)=>{
+  console.error("Failed to Connect")
 })
 
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");  
-});
+
+
+
+
---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose=require("mongoose");
+
+// Url needed to connect to mongodb cluster.
+
+const connectDB= async ()=>{
+  await mongoose.connect("mongodb+srv://mohitkumar98clg_db_user:0kPc4AnRs4ZQhu7a@namastenode.fiangs2.mongodb.net/devTinder");
+};
+
+
+module.exports=connectDB;
+
